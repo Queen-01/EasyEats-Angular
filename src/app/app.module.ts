393f69ff8eccd3d273f6b29a8ce76910d8890490
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-08-05 10:27:08 +0300

    created service file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FirebaseService } from './services/firebase.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(
+      {
+        apiKey: "AIzaSyAHAmCnnHc5WMBIqx2zs3ruwFDzrDvuObI",
+        authDomain: "easy-eats-f5129.firebaseapp.com",
+        projectId: "easy-eats-f5129",
+        storageBucket: "easy-eats-f5129.appspot.com",
+        messagingSenderId: "614125145314",
+        appId: "1:614125145314:web:8921b99b94ecb297f622bb"
+      })
   ],
-  providers: [],
+  providers: [FirebaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
