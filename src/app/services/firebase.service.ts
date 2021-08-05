e4369f4f6e06a29ced6aa049bd5915bdac135c6c
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-08-05 10:35:52 +0300

    added firebase authentication

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import {AngularFireAuth} from '@angular/fire/auth'
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
 
-  constructor() { }
+  isLoggedIn = false
+  constructor(public firebaseAuth : AngularFireAuth) { }
 }
