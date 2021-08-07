a19d7b069b2e3195fec55ab8dd9300aa5ee316be
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-08-07 11:54:45 +0300

    added paths to route module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { AuthGuard } from './auth.service';
+import { EmailComponent } from './email/email.component';
+import { MembersComponent } from './members/members.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'login-email', component: EmailComponent},
+  {path: 'members', component: MembersComponent, canActivate: [AuthGuard]},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
